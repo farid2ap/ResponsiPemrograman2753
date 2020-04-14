3c2fab78fd3aa0ce1393120410a17811e68c94fd
farid2ap <farid.prasetyo@students.amikom.ac.id>
2020-04-14 12:35:14 +0700

    ResponsiPemrograman2753

---
+++ ResponsiPemrograman2753.cs
@@ -0,0 +1,85 @@
+/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Nama : Farid Achmad Prasetyo");
+            Console.WriteLine("NIM  : 19.11.2753");
+            Console.WriteLine("");
+
+            Karyawan karyawan1 = new Karyawan();
+            Karyawan karyawan2 = new Karyawan();
+            Karyawan karyawan3 = new Karyawan();
+
+            karyawan1.No = 1;
+            karyawan1.Nik = 19110001;
+            karyawan1.Nama = "Paijo";
+            karyawan1.Gaji = 3000000;
+
+            karyawan2.No = 2;
+            karyawan2.Nik = 19110002;
+            karyawan2.Nama = "Tarjo";
+            karyawan2.Gaji = 1000000;
+
+            karyawan3.No = 3;
+            karyawan3.Nik = 19110003;
+            karyawan3.Nama = "Tejo";
+            karyawan3.Gaji = -1;
+
+            Console.WriteLine("No /   NIK/Nama/Gaji     ");
+            Console.WriteLine("----------------------------------------");
+            karyawan1.InfoKaryawan();
+            karyawan2.InfoKaryawan();
+            karyawan3.InfoKaryawan();
+            Console.WriteLine("");
+            Console.WriteLine("Selamat Anda Dapat Bonus Insentif 10%");
+            Console.WriteLine("");
+            Console.WriteLine("No /   NIK/Nama/Gaji     ");
+            Console.WriteLine("----------------------------------------");
+            karyawan1.BonusGaji();
+            karyawan2.BonusGaji();
+            karyawan3.BonusGaji();
+	}
+	public class Karyawan
+    {
+        public int No { get; set; }
+        public int Nik { get; set; }
+        public string Nama { get; set; }
+        public int Gaji { get; set; }
+
+       
+
+        public void InfoKaryawan()
+        {
+            if (Gaji < 0)
+            {
+                Gaji = 0;
+            }
+
+            Console.WriteLine("{0}  / {1}/{2}/{3}        ", No, Nik, Nama, Gaji);
+            
+        }
+
+        public void BonusGaji()
+        {
+            if (Gaji < 0)
+            {
+                Gaji = 0;
+            }
+
+            Console.WriteLine("{0}  / {1}/{2}/{3}        ", No, Nik, Nama, Gaji*0.1+Gaji);
+
+        }
+
+
+    }
+}
